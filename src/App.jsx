09f32ab087ec20b40e8fc946762e81f91a2857c4
Toddy Trinh <Toddy.Trinh+ecap@ecapital.com>
2025-11-19 09:58:38 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import { useAcceptJs } from 'react-acceptjs';
+
+const authData = {
+  apiLoginID: '8XaQ8r8Y',
+  clientKey: '8HJeXH2ne7N97A6Xp5S7tJxVex7fp7Wc8JjKQXMa4yX55XquHDCuE58G4z74zwYM',
+};
+
+type BasicCardInfo = {
+  cardNumber: string;
+  cardCode: string;
+  month: string;
+  year: string;
+};
+
+import './App.css'
+
+function App() {
+    const { dispatchData, loading, error } = useAcceptJs({ authData });
+    const [cardData, setCardData] = React.useState<BasicCardInfo>({
+    cardNumber: '',
+    month: '',
+    year: '',
+    cardCode: '',
+  });
+
+  const handleSubmit = async (event) => {
+    console.log("cardData", cardData);
+    // Dispatch CC data to Authorize.net and receive payment nonce for use on your server
+    const response = await dispatchData({ cardData });
+    console.log('Received response:', response);
+  };
+
+  return (
+   <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="cardNumber"
+        value={cardData.cardNumber}
+        onChange={(event) =>
+          setCardData({ ...cardData, cardNumber: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        name="month"
+        value={cardData.month}
+        onChange={(event) =>
+          setCardData({ ...cardData, month: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        name="year"
+        value={cardData.year}
+        onChange={(event) =>
+          setCardData({ ...cardData, year: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        name="cardCode"
+        value={cardData.cardCode}
+        onChange={(event) =>
+          setCardData({ ...cardData, cardCode: event.target.value })
+        }
+      />
+      <button type="submit" disabled={loading || error}>
+        Pay
+      </button>
+    </form>
+  )
+}
+
+export default App
